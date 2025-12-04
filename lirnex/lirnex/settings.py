c571a74d1f4852598e68791bc1b334a3395db896
Andrii Yermakov <gyg283169@gmail.com>
2025-12-04 20:18:48 +0200

    add last versions

--- lirnex/lirnex/settings.py
+++ lirnex/lirnex/settings.py
@@ -139,4 +139,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-ALLOWED_HOSTS = ['.lirnex.com']
+ALLOWED_HOSTS = ['.onrender.com']
